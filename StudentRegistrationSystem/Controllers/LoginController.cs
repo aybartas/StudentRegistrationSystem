2f62c8b4bc8b079e6b419cbb4922b8f4b586de0c
UmurErgenay <64283890+UmurErgenay@users.noreply.github.com>
2020-09-08 17:18:54 +0300

    login falan

---
+++ StudentRegistrationSystem/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentRegistrationSystem.Models.Entity;
+
+namespace StudentRegistrationSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            Admin admin = new Admin
+            {
+                Username = "123",
+                Password = "pass"
+            };
+            return View(admin);
+        }
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
