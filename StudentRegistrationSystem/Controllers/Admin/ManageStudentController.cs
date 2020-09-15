1538c17497dd3729ff6ed96dad15ef4ae492c62b
tunaaybartas <32527396+tunaaybartas@users.noreply.github.com>
2020-09-15 15:34:50 +0300

    layout added

---
+++ StudentRegistrationSystem/Controllers/Admin/ManageStudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistrationSystem.Controllers.Admin
+{
+    public class ManageStudentController : Controller
+    {
+        // GET: ManageStudent
+        public ActionResult List()
+        {
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ StudentRegistrationSystem/ViewModels/StudentRecordViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistrationSystem.ViewModels
+{
+    public class StudentRecordViewModel
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+        public string EducationType { get; set; }
+
+        public string Department { get; set; }
+
+        public string Advisor { get; set; }
+
+        public StudentRecordViewModel(int UserID,string Name, string Lastname, string EducationType,string Department,string Advisor)
+        {
+            this.UserID = UserID;
+            this.Name = Name;
+            this.Lastname = Lastname;
+            this.EducationType = EducationType;
+            this.Department = Department;
+            this.Advisor = Advisor;
+        }
+
+    }
+}
