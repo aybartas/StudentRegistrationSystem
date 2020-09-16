bac2a6317261d00ff298347ff0bbdfc6423bcd67
Sümeyye Turan <sumeyyetrn23@gmail.com>
2020-09-16 10:29:01 +0300

    Select course screen created

---
+++ StudentRegistrationSystem/Controllers/Students/SyllabusController.cs
@@ -0,0 +1,25 @@
+﻿using StudentRegistrationSystem.Models.Context;
+using StudentRegistrationSystem.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistrationSystem.Controllers.Students
+{
+    public class SyllabusController : Controller
+    {
+        StudentRegistrationContext db = new StudentRegistrationContext();
+        // GET: Syllabus
+        public ActionResult Index()
+        {
+            ViewBag.Pazartesi = db.Sections.Where(d => d.Day == "Pazartesi" && d.Lecture.LectureID == d.LectureID).ToList();
+            ViewBag.Salı = db.Sections.Where(d => d.Day == "Salı" && d.Lecture.LectureID == d.LectureID).ToList();
+            ViewBag.Çarsamba = db.Sections.Where(d => d.Day == "Çarşamba" && d.Lecture.LectureID == d.LectureID).ToList();
+            ViewBag.Persembe = db.Sections.Where(d => d.Day == "Perşembe" && d.Lecture.LectureID == d.LectureID).ToList();
+            ViewBag.Cuma = db.Sections.Where(d => d.Day == "Cuma" && d.Lecture.LectureID == d.LectureID).ToList();
+            return View(db.Lectures.ToList());
+        }
+    }
+}
