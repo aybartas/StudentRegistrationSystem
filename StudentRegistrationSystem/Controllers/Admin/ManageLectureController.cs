8ab729366185dd26a3dad946fdbcd5f07458cdff
tunaaybartas <32527396+tunaaybartas@users.noreply.github.com>
2020-09-23 11:36:03 +0300

    managelecturer added

---
+++ StudentRegistrationSystem/Controllers/Admin/ManageLectureController.cs
@@ -0,0 +1,38 @@
+﻿using StudentRegistrationSystem.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistrationSystem.Controllers.Admin
+{
+    public class ManageLectureController : Controller
+    {
+        StudentHelper studentHelper = new StudentHelper();
+        DepartmentHelper departmentHelper = new DepartmentHelper();
+        LecturerHelper lecturerHelper = new LecturerHelper();
+        SectionHelper sectionHelper = new SectionHelper();
+        EnrollmentHelper enrollmentHelper = new EnrollmentHelper();
+
+        // GET: ManageLecture
+        public ActionResult List()
+        {
+
+            return View();
+        }
+
+        public ActionResult AddLecture()
+        {
+
+            return View();
+        }
+
+        public ActionResult AddSection()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ StudentRegistrationSystem/ViewModels/ListLecturesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StudentRegistrationSystem.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegistrationSystem.ViewModels
+{
+    public class ListLecturesViewModel
+    {
+        public List<Lecture> lectures { get; set; }
+       
+    }
+}
