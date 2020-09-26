41b611619b41d2f2ae9e7a2e18cb08ffb6d9da41
UmurErgenay <64283890+UmurErgenay@users.noreply.github.com>
2020-09-26 22:57:29 +0300

    Epey bir şey var işte

---
+++ StudentRegistrationSystem/Controllers/Students/DepartmentInfoController.cs
@@ -0,0 +1,21 @@
+﻿using StudentRegistrationSystem.Helpers;
+using StudentRegistrationSystem.Models.Entity;
+using StudentRegistrationSystem.ViewModels.AdminLecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace StudentRegistrationSystem.Controllers.Students
+{
+    public class DepartmentInfoController: Controller
+    {
+        public ActionResult All()
+        {
+
+            return View();
+        }
+    }
+}
